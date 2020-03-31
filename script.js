4cfba3c4840101344ace091765984c55d330fe99
Wanda Antoine <wanda@thinkful.com>
2020-03-30 21:21:08 -0400

    changed yelp api (cors disabled) to you tube api fixed GET requests  and tested responses in the console

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 'use strict';
 
-
-
-
 //functions for the spoonacular Api
 function formatQueryParams(ingredient, mealType, dietType, allergies) {
     console.log('formatQueryParams()');
@@ -11,13 +8,12 @@ function formatQueryParams(ingredient, mealType, dietType, allergies) {
     queryString += `type=${mealType}&`;
     queryString += `diet=${dietType}&`;
     queryString += `intolerances=${allergies}&`;
-    console.log(queryString)
+    // console.log(queryString)
     return queryString
 }
 
 function getRecipes(ingredient, mealType, dietType, allergies) {
     console.log('getRecipes()');
-    // urls and api-keys of both Apis
     const apiKeyspoonacular = "0a704e80601d4fbe8ef5821111aa6479";
     const searchUrlSpoonacular = "https://api.spoonacular.com/recipes/complexSearch";
 
@@ -25,59 +21,98 @@ function getRecipes(ingredient, mealType, dietType, allergies) {
     const url = searchUrlSpoonacular + '?' + queryString + 'maxCalories=500&number=20&instructionsRequired=true&addRecipeInformation=true' + `&apiKey=${apiKeyspoonacular}`;
     console.log(url);
 
-
     fetch(url)
-        .then(response => response.json())
-        .then(responseJson => console.log(responseJson));
-}
-
-//functions for the yelp Api
-function getRestaurants() {
-    console.log('getRestaurants()');
-
-    const options = {
-        headers: new Headers({
-            Authorization: 'Bearer API_KEY'
-        })
-    };
-
-    const apiKeyYelp = 'AuZRmmpJYgg6UylAgbTMRLnQHly1vo6kcPe3SQzFd-3eEfxrRVGcbmSTT8AlCnsomgPjvfSP7_jM7uNCpDtbNQAK30Bo4Z_l1P6DlqmavIlGyWo6MnO51nFmstd7XnYx';
-    // const urlYelp = `https://api.yelp.com/v3/businesses/search?location=${location}`;
-    const newUrl = 'https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972';
-
-    fetch(newUrl, {
-        dataType: 'jsonp',
-        crossDomain: true,
-        headers: {
-            // 'cors' by default
-            'Access-Control-Allow-Origin': "*",
-            "Accept": "application/jsonp",
-            "Content-Type": "application/jsonp",
-            mode: 'no-cors',
-            Authorization: `Bearer ${apiKeyYelp}`,
-            'API_KEY': apiKeyYelp
-        }
-    })
-
-        //success scenario (call the function to display the results)
         .then(response => {
-            console.log(response);
             if (response.ok) {
                 return response.json();
             }
             throw new Error(response.statusText);
         })
         .then(responseJson => console.log(responseJson))
-
-        // failure scenario (display errors)
         .catch(err => {
-            console.log(err);
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
         });
-
-    // fetch(urlYelp, options)
-    //     .then(response => response.json())
-    //     .then(responseJson => console.log(responseJson));
 }
+
+//functions for the youtube Api
+
+
+function formatQueryParamsYoutube() {
+    const API_KEY = 'AIzaSyBslEsiJJgKJb6bak269C49LaArNjU4xxc';
+    const ingredient = $("#js-search-ingredient").val();
+    const mealType = $("#js-search-meal").val();
+    const dietType = $("#js-search-diet").val();
+    const allergies = $("#js-search-allergies").val();
+
+
+    console.log('formatQueryParamsYoutube()');
+    let queryString = '';
+    queryString += `key=${API_KEY}&`;
+    queryString += `q=${ingredient}+${mealType}+${dietType}+${allergies}+recipe&`;
+    queryString += 'part=snippet&';
+    queryString += 'maxResults=6';
+    console.log(queryString)
+    return queryString
+}
+
+function getYouTubeVideos() {
+    // const searchURL = `https://www.googleapis.com/youtube/v3/search?q=${ingredient}+recipe&`;
+    const searchURL = `https://www.googleapis.com/youtube/v3/search`;
+    // const searchURL = 'https://www.googleapis.com/youtube/v3/search?q=strawberry+recipies&';
+    // GET https://www.googleapis.com/youtube/v3/search?part=snippet&q=strawberry%20recipe&key=[YOUR_API_K;
+
+    // const params = {
+    //     key: API_KEY,
+    //     q: `${ingredient}`,
+    //     part: 'snippet',
+    //     maxResults: 10
+    // };
+
+    // function formatQueryParamsYoutube(params) {
+    //     const queryItems = Object.keys(params)
+    //         .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    //     return queryItems.join('&');
+    // }
+
+    const queryStringYoutube = formatQueryParamsYoutube()
+    const urlYouTube = searchURL + '?' + queryStringYoutube;
+    console.log(urlYouTube);
+
+    fetch(urlYouTube)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => console.log(responseJson))
+        .catch(err => {
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        });
+}
+
+// function displayResultsSpoonacular(responseJson) {
+//     console.log('displayResults()');
+//     $('#results-recipes-list').empty();
+
+//     for (let i = 0; i < responseJson.data.length; i++) {
+//         // console.log(responseJson.data[i]);
+//         // console.log(responseJson.data[i].addresses[i]);
+//         $('#results-list').append(
+//             `<li>
+//                 <p>Park Name: "${responseJson.data[i].fullName}"</p>
+//                 <p>Description: "${responseJson.data[i].description}"</p>
+//                 <p>Url: <a  href="${responseJson.data[i].url}">"${responseJson.data[i].url}"</a></p>
+          
+//             </li>`
+//             // <p>Addresses: "${responseJson.data[i].addresses[i]}"</p>
+//         )
+//     };
+//     $('#results').removeClass('hidden');
+// };
+
+
+
 
 //eventhandler for both spoonacular and yelp Apis
 function watchForm() {
@@ -88,15 +123,16 @@ function watchForm() {
         const mealType = $("#js-search-meal").val();
         const dietType = $("#js-search-diet").val();
         const allergies = $("#js-search-allergies").val();
-        const location = $("#js-location").val();
+        // const videos = $("#js-search-ingredient").val();
+        const maxResults = $("#js-max-results").val();
         getRecipes(ingredient, mealType, dietType, allergies);
-        console.log(ingredient, mealType, dietType, allergies);
-        getRestaurants();
-        console.log(location);
+        console.log(`${ingredient}`);
+        getYouTubeVideos();
     });
 }
 
 $(function () {
     console.log('App loaded! Waiting for submit!');
     watchForm();
+    displayResultsSpoonacular();
 });
